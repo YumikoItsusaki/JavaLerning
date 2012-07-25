fec5f0e42d666a70f3b6f7460575bc7d088c00fb
itacademy <itacademy@192.168.1.8>
2012-07-25 21:42:16 +0900

    e-j

--- src/jp/itacademy/samples/api/concurrent/Start.java
+++ src/jp/itacademy/samples/api/concurrent/Start.java
@@ -1,30 +1,22 @@
 package jp.itacademy.samples.api.concurrent;
 
 public class Start {
-	
+
 	private String userName;
 	private String userSign;
-	
+
 	public void hold(String userName) {
 		this.userName = userName;
 		this.userSign = userName;
 		checkSign();
 		System.out.println(this.userName + "さんが座っています");
-		
-		private void checkSign() {
-			if (userName.charAt(0) != userSign.charAt(0)) {
-				System.err.println("サインと名前がちがいますが..." + " name:" + userName + " sign:" + userSign);
-				
-			}
-		}
-		
 	}
 
 	private void checkSign() {
-		// TODO 自動生成されたメソッド・スタブ
-		
+		if (userName.charAt(0) != userSign.charAt(0)) {
+			System.err.println("サインと名前がちがいますが..." + " name:" + userName
+					+ " sign:" + userSign);
+		}
 	}
-	
-
 
 }
--- src/jp/itacademy/samples/ensyu/Kazuate.java
+++ src/jp/itacademy/samples/ensyu/Kazuate.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class Kazuate {
 	public static void main(String[] args) {
 		Random rnd = new Random();
-		//int count;
+		int count = 0;
 
 		int ran = rnd.nextInt(100) + 1;
-		//count=0;ran=Random;count++
 		while (true) {
 			System.out.println("1から100までの数字を入力してください");
 
 			Scanner scanner = new Scanner(System.in);
 			int nu = scanner.nextInt();
+			count ++; 
 
 			if (nu == ran) {
 				System.out.println("正解です！");
-				System.out.println("　+ cut + 回目で正解でした。");
+				System.out.println(count + "回目で正解でした。");
 				break;
 			}
-		if (nu > ran) {
-			System.out.println("もっと小さいです");
+			if (nu > ran) {
+				System.out.println("もっと小さいです");
+			}
+			if (nu < ran) {
+				System.out.println("もっと大きいです");
+			}
 		}
-		if (nu < ran) {
-			System.out.println("もっと大きいです");
-		}
-	}
 	}
 }
